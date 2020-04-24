102e6c57cb3bd3b4aa77b153cb47fa3cc1d2f924
CppPhil <CppPhil@users.noreply.github.com>
2020-04-24 23:42:53 +0200

    node_a REMOTE main

--- node_a/src/main.cpp
+++ node_a/src/main.cpp
@@ -1,5 +1,9 @@
+#include <cstdio>
+
 #include <caf/all.hpp>
 #include <caf/io/all.hpp>
+
+#include <fmt/format.h>
 
 #include "log.hpp"
 #include "test_actor_buddy_function.hpp"
@@ -20,6 +24,23 @@ struct config : caf::actor_system_config {
 } // namespace
 
 void caf_main(caf::actor_system& sys, const config& config) {
+#ifdef REMOTE
+  L_LOG("REMOTE");
+  const auto expected_remote_actor = caf::io::remote_actor(sys, "0.0.0.0",
+                                                           1337);
+
+  if (!expected_remote_actor) {
+    fmt::print(stderr, "Could not connect to remote actor!\n");
+    L_LOG("Could not connect to remote actor!");
+    return;
+  }
+
+  const auto& remote_actor = *expected_remote_actor;
+
+  L_LOG("About to spawn test_actor_buddy_function");
+  sys.spawn(&l::test_actor_buddy_function, remote_actor);
+  L_LOG("Spawned test_actor_buddy_function");
+#else
   L_LOG("About to spawn test_actor_function");
   auto test_actor = sys.spawn(&l::test_actor_function);
   L_LOG("Spawned test_actor_function");
@@ -27,6 +48,7 @@ void caf_main(caf::actor_system& sys, const config& config) {
   L_LOG("About to spawn test_actor_buddy_function");
   sys.spawn(&l::test_actor_buddy_function, test_actor);
   L_LOG("Spawned test_actor_buddy_function");
+#endif
 }
 
 CAF_MAIN(caf::io::middleman)
