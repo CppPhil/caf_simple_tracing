fa31a5019f0b86e180018eacba65ec159eb955ff
CppPhil <CppPhil@users.noreply.github.com>
2020-04-24 18:28:37 +0200

    format

--- lib/include/log.hpp
+++ lib/include/log.hpp
@@ -10,26 +10,20 @@
 #include <pl/current_function.hpp>
 #include <pl/string_view.hpp>
 
-namespace l
-{
-namespace detail
-{
-class logger
-{
+namespace l {
+namespace detail {
+class logger {
 public:
   static logger& instance();
 
-  template <class ...Ts>
-  void log(
-    pl::string_view file,
-    int64_t line,
-    pl::string_view function,
-    const Ts&... xs)
-  {
+  template <class... Ts>
+  void log(pl::string_view file, int64_t line, pl::string_view function,
+           const Ts&... xs) {
     std::lock_guard<std::mutex> lock(mutex_);
-    ofs_ << fmt::format("file: {}\nline: {}\nfunction: {}\n", file.data(), line, function.line());
+    ofs_ << fmt::format("file: {}\nline: {}\nfunction: {}\n", file.data(), line,
+                        function.line());
     ofs_ << "message: " << fmt::format(xs...) << '\n' << std::endl;
-  } 
+  }
 
 private:
   explicit logger(pl::string_view log_file_path);
@@ -40,11 +34,6 @@ private:
 } // namespace detail
 } // namespace l
 
-#define L_LOG(...) \
-  ::l::detail::logger::instance().log( \
-  __FILE__, \
-  __LINE__, \
-  PL_CURRENT_FUNCITON, \
-  __VA_ARGS__ \
-)
-
+#define L_LOG(...)                                                             \
+  ::l::detail::logger::instance().log(__FILE__, __LINE__, PL_CURRENT_FUNCTION, \
+                                      __VA_ARGS__)
--- lib/src/log.cpp
+++ lib/src/log.cpp
@@ -1,21 +1,16 @@
 #include "log.hpp"
 
-namespace l
-{
-namespace detail
-{
-logger& logger::instance()
-{
+namespace l {
+namespace detail {
+logger& logger::instance() {
   static logger log_instance("simple_caf_tracing.log");
   return log_instance;
 }
 
 logger::logger(pl::string_view log_file_path)
   : ofs_(log_file_path.data(), std::ios_base::out | std::ios_base::trunc),
-    mutex_()
-{
+    mutex_() {
   // nop
 }
-} // naemspace detail
+} // namespace detail
 } // namespace l
-
