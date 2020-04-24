bf0a7e26a9893d3c2ad4ac17e95148bd6db7c2a5
CppPhil <CppPhil@users.noreply.github.com>
2020-04-24 22:57:14 +0200

    Do basic main

--- node_a/src/main.cpp
+++ node_a/src/main.cpp
@@ -1,5 +1,25 @@
-#include "log.hpp"
+#include <caf/all.hpp>
 
-int main() {
-  L_LOG("This is a log entry {} {} ", 1, 2);
+#include "test_actor_buddy_function.hpp"
+#include "test_actor_function.hpp"
+#include "test_profiler.hpp"
+#include "test_tracing_data_factory.hpp"
+
+namespace {
+struct config : caf::actor_system_config {
+  l::test_profiler test_profiler;
+  l::test_tracing_data_factory test_tracing_context;
+
+  config() {
+    profiler = &test_profiler;
+    tracing_context = &test_tracing_context;
+  }
+};
+} // namespace
+
+void caf_main(caf::actor_system& sys, const config& config) {
+  auto test_actor = sys.spawn(&l ::test_actor_function);
+  sys.spawn(&l ::test_actor_buddy_function, test_actor);
 }
+
+CAF_MAIN()
