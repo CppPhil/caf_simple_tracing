aa2233662d8a89bff60496880fc95b105c680320
CppPhil <CppPhil@users.noreply.github.com>
2020-04-24 18:27:13 +0200

    Basic log

---
+++ lib/include/log.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include <cstdint>
+
+#include <fstream>
+#include <mutex>
+
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+
+#include <pl/current_function.hpp>
+#include <pl/string_view.hpp>
+
+namespace l
+{
+namespace detail
+{
+class logger
+{
+public:
+  static logger& instance();
+
+  template <class ...Ts>
+  void log(
+    pl::string_view file,
+    int64_t line,
+    pl::string_view function,
+    const Ts&... xs)
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    ofs_ << fmt::format("file: {}\nline: {}\nfunction: {}\n", file.data(), line, function.line());
+    ofs_ << "message: " << fmt::format(xs...) << '\n' << std::endl;
+  } 
+
+private:
+  explicit logger(pl::string_view log_file_path);
+
+  std::ofstream ofs_;
+  std::mutex mutex_;
+};
+} // namespace detail
+} // namespace l
+
+#define L_LOG(...) \
+  ::l::detail::logger::instance().log( \
+  __FILE__, \
+  __LINE__, \
+  PL_CURRENT_FUNCITON, \
+  __VA_ARGS__ \
+)
+
---
+++ lib/src/log.cpp
@@ -0,0 +1,21 @@
+#include "log.hpp"
+
+namespace l
+{
+namespace detail
+{
+logger& logger::instance()
+{
+  static logger log_instance("simple_caf_tracing.log");
+  return log_instance;
+}
+
+logger::logger(pl::string_view log_file_path)
+  : ofs_(log_file_path.data(), std::ios_base::out | std::ios_base::trunc),
+    mutex_()
+{
+  // nop
+}
+} // naemspace detail
+} // namespace l
+
