ea4b1b08ab3957915d584f6114718ce2099f4fe1
CppPhil <CppPhil@users.noreply.github.com>
2020-04-24 22:34:07 +0200

    get_tracing_data

--- lib/include/test_tracing_data.hpp
+++ lib/include/test_tracing_data.hpp
@@ -1,4 +1,8 @@
 #pragma once
+#include <cstdio>
+
+#include <fmt/format.h>
+
 #include <caf/all.hpp>
 
 namespace l {
@@ -13,4 +17,31 @@ public:
   caf::error_code<caf::sec>
   serialize(caf::binary_serializer& sink) const override;
 };
+
+template <class T>
+test_tracing_data* get_tracing_data(T& actor) {
+  auto* mailbox_element = actor->current_mailbox_element();
+
+  if (mailbox_element == nullptr) {
+    fmt::print(stderr,
+               "get_tracing_data: Couldn't get mailbox_element, WTF?????\n");
+    return nullptr;
+  }
+
+  auto* tracing_id = mailbox_element->tracing_id.get();
+
+  if (tracing_id == nullptr) {
+    fmt::print(stderr, "get_tracing_data: Couldn't get tracing_id!!!\n");
+    return nullptr;
+  }
+
+  auto* downcasted = dynamic_cast<test_tracing_data*>(tracing_id);
+
+  if (downcasted == nullptr) {
+    fmt::print(stderr, "get_tracing_data: Couldn't downcast, WTF????\n");
+    return nullptr;
+  }
+
+  return downcasted;
+}
 } // namespace l
